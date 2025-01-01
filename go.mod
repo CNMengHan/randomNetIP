e3f7a65ffbf4ef005a15e4e8963ae1412dbbc47a
CNMengHan <120065153+CNMengHan@users.noreply.github.com>
2025-01-02 05:34:26 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CNMengHan/randomNetIP
+
+go 1.22.7
---
+++ randomNetIP.go
@@ -0,0 +1,38 @@
+package randomNetIP
+
+import (
+	"math/rand"
+	"net"
+)
+
+func randomNetIPv4() string {
+	privateRanges := []string{
+		"10.0.0.0/8",
+		"172.16.0.0/12",
+		"192.168.0.0/16",
+		"169.254.0.0/16",
+		"100.64.0.0/10",
+		"127.0.0.0/8",
+	}
+
+	for {
+		firstOctet := rand.Intn(223-1) + 1
+		secondOctet := rand.Intn(256)
+		thirdOctet := rand.Intn(256)
+		fourthOctet := rand.Intn(256)
+
+		ip := net.IPv4(byte(firstOctet), byte(secondOctet), byte(thirdOctet), byte(fourthOctet)).String()
+
+		isPrivate := false
+		for _, cidr := range privateRanges {
+			_, ipNet, _ := net.ParseCIDR(cidr)
+			if ipNet.Contains(net.ParseIP(ip)) {
+				isPrivate = true
+				break
+			}
+		}
+		if !isPrivate {
+			return ip
+		}
+	}
+}
